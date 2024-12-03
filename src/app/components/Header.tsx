4d8bac24ec28b722cb9a9b7a504fb2454d9bbeb3
河﨑晴己 <kawasakih@scala-partners.com>
2024-12-03 11:13:11 +0900

    change: デザイン微調整

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Header() {
   return (
-    <header className="bg-blue-600 text-white p-4">
+    <header className="bg-red-400 text-white p-4">
       <h1 className="text-2xl font-bold">どしたん話聞こか</h1>
     </header>
   );
